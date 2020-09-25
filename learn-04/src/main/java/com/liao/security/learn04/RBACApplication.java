968277dd5e043180dbdd7f3efea6ad912fcef2b3
liaoxianfu <liaoxianfu555@outlook.com>
2020-09-25 11:25:19 +0800

    修正包名错误

---
+++ learn-04/src/main/java/com/liao/security/learn04/RBACApplication.java
@@ -0,0 +1,16 @@
+package com.liao.security.learn04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liao
+ * @since 2020/9/25 10:35
+ */
+
+@SpringBootApplication
+public class RBACApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RBACApplication.class, args);
+    }
+}
